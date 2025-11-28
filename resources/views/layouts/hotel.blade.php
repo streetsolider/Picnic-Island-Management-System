6bcbf91b1e64a2b4119d927e8e59cd3e5eec818b
Ismail Wasiu Abdul Samad (UWE ID: 24050765) <Ismail2.Abdulsamad@live.uwe.ac.uk>
2025-11-28 06:01:35 +0500

    Update hotel.blade.php

--- resources/views/layouts/hotel.blade.php
+++ resources/views/layouts/hotel.blade.php
@@ -38,40 +38,6 @@
             {{ __('Dashboard') }}
         </x-admin.navigation.nav-link>
 
-        {{-- Room Management Section --}}
-        <div class="mt-4 px-3">
-            <h3 class="px-3 text-xs font-semibold text-gray-500 dark:text-gray-400 uppercase tracking-wider">
-                {{ __('Room Management') }}
-            </h3>
-        </div>
-
-        <x-admin.navigation.nav-link :href="route('hotel.rooms.index')" :active="request()->routeIs('hotel.rooms.*')"
-            icon='<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"></path></svg>'>
-            {{ __('All Rooms') }}
-        </x-admin.navigation.nav-link>
-
-        <x-admin.navigation.nav-link :href="route('hotel.views.manage')" :active="request()->routeIs('hotel.views.*')"
-            icon='<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path></svg>'>
-            {{ __('Room Views') }}
-        </x-admin.navigation.nav-link>
-
-        {{-- Amenities Section --}}
-        <div class="mt-4 px-3">
-            <h3 class="px-3 text-xs font-semibold text-gray-500 dark:text-gray-400 uppercase tracking-wider">
-                {{ __('Amenities') }}
-            </h3>
-        </div>
-
-        <x-admin.navigation.nav-link :href="route('hotel.amenities.categories')" :active="request()->routeIs('hotel.amenities.categories')"
-            icon='<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 10h16M4 14h16M4 18h16"></path></svg>'>
-            {{ __('Categories') }}
-        </x-admin.navigation.nav-link>
-
-        <x-admin.navigation.nav-link :href="route('hotel.amenities.items')" :active="request()->routeIs('hotel.amenities.items')"
-            icon='<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"></path></svg>'>
-            {{ __('Amenity Items') }}
-        </x-admin.navigation.nav-link>
-
         {{-- Top Bar --}}
         <x-slot:topBar>
             @if (isset($header))
