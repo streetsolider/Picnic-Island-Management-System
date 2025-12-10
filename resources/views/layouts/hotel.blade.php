70d11d310fe76e8122583fe975e66dc49912a76a
Ismail Wasiu Abdul Samad (UWE ID: 24050765) <Ismail2.Abdulsamad@live.uwe.ac.uk>
2025-12-10 16:38:39 +0500

    Update hotel.blade.php

--- resources/views/layouts/hotel.blade.php
+++ resources/views/layouts/hotel.blade.php
@@ -108,29 +108,6 @@
                 icon='<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>'>
                 {{ __('Settings') }}
             </x-admin.navigation.nav-link>
-
-            {{-- Reports & Analytics Section --}}
-            <div class="mt-4 px-3">
-                <h3 class="px-3 text-xs font-semibold text-gray-500 dark:text-gray-400 uppercase tracking-wider">
-                    {{ __('Reports & Analytics') }}
-                </h3>
-            </div>
-
-            {{-- Coming Soon --}}
-            {{-- <x-admin.navigation.nav-link :href="route('hotel.reports.occupancy')" :active="request()->routeIs('hotel.reports.occupancy')"
-                icon='<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"></path></svg>'>
-                {{ __('Occupancy Reports') }}
-            </x-admin.navigation.nav-link> --}}
-
-            {{-- <x-admin.navigation.nav-link :href="route('hotel.reports.revenue')" :active="request()->routeIs('hotel.reports.revenue')"
-                icon='<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>'>
-                {{ __('Revenue Analytics') }}
-            </x-admin.navigation.nav-link> --}}
-
-            {{-- <x-admin.navigation.nav-link :href="route('hotel.reports.bookings')" :active="request()->routeIs('hotel.reports.bookings')"
-                icon='<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"></path></svg>'>
-                {{ __('Booking History') }}
-            </x-admin.navigation.nav-link> --}}
         @endif
 
         {{-- Top Bar --}}
